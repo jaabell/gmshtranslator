78a72f26eae26131aaa16213cb598591ec5ff78b
amanabt <aman.iitk072@gmail.com>
2017-02-02 23:45:23 +0530

    Modified the files to run on python3

--- gmshtranslator/__init__.py
+++ gmshtranslator/__init__.py
@@ -1,5 +1,5 @@
-print "gmshtranslator friendly reminder: rules and actions prototypes\n\n"
-print "def node_condition(tag,x,y,z,physgroups): "
-print "def node_action(tag,x,y,z):"
-print "def element_condition(eletag,eletype,physgrp,nodes):"
-print "def element_action(eletag,eletype,physgrp,nodes):"
+print ("gmshtranslator friendly reminder: rules and actions prototypes\n\n")
+print ("def node_condition(tag,x,y,z,physgroups): ")
+print ("def node_action(tag,x,y,z):")
+print ("def element_condition(eletag,eletype,physgrp,nodes):")
+print ("def element_action(eletag,eletype,physgrp,nodes):")
