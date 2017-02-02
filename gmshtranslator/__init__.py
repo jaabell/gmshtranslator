ea93b66393de0f8215801d7efd21efaed1c917c8
amanabt <aman.iitk072@gmail.com>
2017-02-02 23:51:19 +0530

    This program is compatible with both python3 and python2.

--- gmshtranslator/__init__.py
+++ gmshtranslator/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 print ("gmshtranslator friendly reminder: rules and actions prototypes\n\n")
 print ("def node_condition(tag,x,y,z,physgroups): ")
 print ("def node_action(tag,x,y,z):")
